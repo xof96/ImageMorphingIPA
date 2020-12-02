ba826a9f221b430c76b6b80fdd3a4170eb5b98e5
Matías Pizarro <matias.pizarro.o96@gmail.com>
2020-12-02 18:40:01 -0300

    Requirements modified. Main file loads the images, the lines file and does some preprocessing

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+from utils.utils import imread
+import matplotlib.pyplot as plt
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+img_o_path = 'assets/glass/vaso.jpg'
+img_d_path = 'assets/glass/copa.jpg'
 
+img_origin = imread(filename=img_o_path)
+if img_origin.shape[2] > 3:
+    img_origin = img_origin[:, :, 0:3]
+img_destination = imread(filename=img_d_path)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+o_shape = img_origin.shape
+d_shape = img_destination.shape
+# %%
+t = 0.5
+buf_shape = []
+for i in range(3):
+    buf_shape.append(max(o_shape[i], d_shape[i]))
 
+# if __name__ == '__main__':
+# %%
+n_lines = 0
+pa = []
+qa = []
+pb = []
+qb = []
+with open('data/glass_lines.txt', 'r') as ref_lines:
+    lines = ref_lines.readlines()
+    for line in lines:
+        n_lines += 1
+        if line[-1] == '\n':
+            line = line[0: -1]
+        coords = line.split(',')
+        curr_pa = (int(coords[0]), int(coords[1]))
+        curr_qa = (int(coords[2]), int(coords[3]))
+        curr_pb = (int(coords[4]), int(coords[5]))
+        curr_qb = (int(coords[6]), int(coords[7]))
+        pa.append(curr_pa)
+        qa.append(curr_qa)
+        pb.append(curr_pb)
+        qb.append(curr_qb)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Calcuating PQ and PQ'
+pqa = []
+pqb = []
+for i in range(n_lines):
+    pqa.append((qa[i][0] - pa[i][0], qa[i][1] - pa[i][1]))
+    pqb.append((qb[i][0] - pb[i][0], qb[i][1] - pb[i][1]))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+rows, cols, _ = buf_shape
+for i in range(rows):
+    for j in range(cols):
